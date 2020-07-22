b407b13f8815d11343c8f0ec74f3a2c2f1bd401c
McKayla Lankau <lankaukk@yahoo.com>
2020-07-22 20:51:52 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,7 @@
 def turn_count(board)
   turns = 0
   board.each do |element|
-    if board[turns] == "O" ||
-    if board[turns] == "X"
+    if element == "X" || element == "O"
     turns += 1
     end
   end
