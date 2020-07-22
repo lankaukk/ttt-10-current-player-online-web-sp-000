657f9b66b374de394c29ee5e6727e27bef94d09d
McKayla Lankau <lankaukk@yahoo.com>
2020-07-22 21:00:23 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,7 +8,7 @@ def turn_count(board)
   turns
 end
 
-def current_player(turn_count)
+def current_player(turn_count, board)
   if turn_count(board) % 2 == 0
     return "X"
   elsif turn_count(board) % 2 == 1 
