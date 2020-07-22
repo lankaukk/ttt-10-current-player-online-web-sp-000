98918519d5d704f0089d35a111bcca5eda81276c
McKayla Lankau <lankaukk@yahoo.com>
2020-07-22 19:42:17 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  # returns the number of turns that have been played
+  counter = 0
+  board.each do |turns|
+    puts "This is turn number #{turns}"
+    if board[counter] = "X" || "O" 
+    counter += 1
+    return counter
+  end
+  end
+end
+
+def current_player(board, turn_count)
+  if turn_count % 2 == 0
+    return "X"
+  else return "O"
+  end
+end
